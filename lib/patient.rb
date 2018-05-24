5b8fe437e4b97ab5a718cab38853cbff825a02f9
Kayvan Vakili <kvakili813@gmail.com>
2018-05-24 19:46:55 +0000

    automatically backed up by learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -9,5 +9,13 @@ attr_accessor :name
     @@all << self
   end
 
+  def appointments
+    Appointment.all.select {|appointments| appointments.patient == self}
+  end
+
+  def new_appointment(doctor, date)
+    Appointment.new
+    
+  end
 
 end
